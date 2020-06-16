dcc5f9e3d7b1d1a31c658a2e35e8a7f234447eb7
Xie Zhihao <xzh1206@gmail.com>
2020-06-17 00:20:21 +0800

    Seperate cachers into another module

---
+++ src/cacher/mod.rs
@@ -0,0 +1,274 @@
+use std::cmp::{max, min};
+use std::collections::BTreeMap;
+use std::io;
+use std::ops::Bound::Included;
+
+/// Represents the initial size of cache.
+const INITIAL_CACHE_SIZE: usize = 65536;
+/// Represents the max size of cache.
+const MAX_CACHE_SIZE: usize = 16 * INITIAL_CACHE_SIZE;
+
+/// Represents the max distance of u32 values between packets in an u32 window.
+const MAX_U32_WINDOW_SIZE: usize = 4194304;
+
+/// Represents the linear cache.
+#[derive(Debug)]
+pub struct Cacher {
+    buffer: Vec<u8>,
+    sequence: u32,
+    head: usize,
+    size: usize,
+}
+
+impl Cacher {
+    /// Creates a new `Cacher`.
+    pub fn new(sequence: u32) -> Cacher {
+        Cacher {
+            buffer: vec![0; INITIAL_CACHE_SIZE],
+            sequence,
+            head: 0,
+            size: 0,
+        }
+    }
+
+    /// Get the buffer in the given size. The returned vector's length will be no more than the given size.
+    pub fn get(&self, size: usize) -> Option<Vec<u8>> {
+        if self.size == 0 {
+            return None;
+        }
+
+        let length = min(self.size, size);
+        let mut vector = vec![0u8; length];
+
+        // From the head to the end of the buffer
+        let length_a = min(length, self.buffer.len() - self.head);
+        vector[..length_a].copy_from_slice(&self.buffer[self.head..self.head + length_a]);
+
+        // From the begin of the buffer to the tail
+        let length_b = length - length_a;
+        if length_b > 0 {
+            vector[length_a..].copy_from_slice(&self.buffer[..length_b]);
+        }
+
+        Some(vector)
+    }
+
+    /// Appends some bytes to the end of the cache.
+    pub fn append(&mut self, buffer: &[u8]) -> io::Result<()> {
+        if buffer.len() > self.buffer.len() - self.size {
+            // Extend the buffer
+            let size = min(
+                max(self.buffer.len() * 2, self.buffer.len() + buffer.len()),
+                MAX_CACHE_SIZE,
+            );
+            if self.size + buffer.len() > size {
+                return Err(io::Error::new(io::ErrorKind::Other, "cache is full"));
+            }
+
+            let mut new_buffer = vec![0u8; size];
+
+            // From the head to the end of the buffer
+            let length_a = min(self.size, self.buffer.len() - self.head);
+            new_buffer[..length_a].copy_from_slice(&self.buffer[self.head..self.head + length_a]);
+
+            // From the begin of the buffer to the tail
+            let length_b = self.size - length_a;
+            if length_b > 0 {
+                new_buffer[length_a..length_a + length_b].copy_from_slice(&self.buffer[..length_b]);
+            }
+
+            self.buffer = new_buffer;
+            self.head = 0;
+        }
+
+        // From the tail to the end of the buffer
+        let mut length_a = 0;
+        if self.head + self.size < self.buffer.len() {
+            length_a = min(buffer.len(), self.buffer.len() - (self.head + self.size));
+            self.buffer[self.head + self.size..self.head + self.size + length_a]
+                .copy_from_slice(&buffer[..length_a]);
+        }
+
+        // From the begin of the buffer to the head
+        let length_b = buffer.len() - length_a;
+        if length_b > 0 {
+            self.buffer[..length_b].copy_from_slice(&buffer[length_a..]);
+        }
+
+        self.size += buffer.len();
+
+        Ok(())
+    }
+
+    // Invalidates cache to the certain sequence.
+    pub fn invalidate_to(&mut self, sequence: u32) {
+        let size = sequence
+            .checked_sub(self.sequence)
+            .unwrap_or_else(|| u32::MAX - self.sequence + sequence) as usize;
+
+        if size <= MAX_U32_WINDOW_SIZE as usize {
+            self.sequence = sequence;
+            self.size = self.size.checked_sub(size).unwrap_or(0);
+            if self.size == 0 {
+                self.head = 0;
+            } else {
+                self.head = (self.head + (size % self.buffer.len())) % self.buffer.len();
+            }
+        }
+    }
+}
+
+/// Represents the random cache.
+#[derive(Debug)]
+pub struct RandomCacher {
+    buffer: Vec<u8>,
+    sequence: u32,
+    head: usize,
+    /// Represents ranges of existing values. Use an u64 instead of an u32 because the sequence is used as a ring.
+    ranges: BTreeMap<u64, usize>,
+}
+
+impl RandomCacher {
+    /// Creates a new `RandomCacher`.
+    pub fn new(sequence: u32) -> RandomCacher {
+        RandomCacher {
+            buffer: vec![0u8; INITIAL_CACHE_SIZE],
+            sequence,
+            head: 0,
+            ranges: BTreeMap::new(),
+        }
+    }
+
+    /// Appends some bytes to the cache and returns continuous bytes from the beginning.
+    pub fn append(&mut self, buffer: &[u8], sequence: u32) -> io::Result<Option<Vec<u8>>> {
+        let sub_sequence = sequence
+            .checked_sub(self.sequence)
+            .unwrap_or_else(|| sequence + (u32::MAX - self.sequence))
+            as usize;
+        if sub_sequence > MAX_U32_WINDOW_SIZE {
+            return Ok(None);
+        }
+
+        let size = sub_sequence + buffer.len();
+        if size > self.buffer.len() {
+            // Extend the buffer
+            let size = min(max(self.buffer.len() * 2, size), MAX_CACHE_SIZE);
+            if self.buffer.len() + buffer.len() > size {
+                return Err(io::Error::new(io::ErrorKind::Other, "cache is full"));
+            }
+
+            let mut new_buffer = vec![0u8; size];
+
+            // TODO: the procedure may by optimized to copy valid bytes only
+            // From the head to the end of the buffer
+            new_buffer[..self.buffer.len() - self.head].copy_from_slice(&self.buffer[self.head..]);
+
+            // From the begin of the buffer to the tail
+            if self.head > 0 {
+                new_buffer[self.buffer.len() - self.head..self.buffer.len()]
+                    .copy_from_slice(&self.buffer[..self.head]);
+            }
+
+            self.buffer = new_buffer;
+            self.head = 0;
+        }
+
+        // TODO: the procedure may by optimized to copy valid bytes only
+        // To the end of the buffer
+        let mut length_a = 0;
+        if self.buffer.len() - self.head > sub_sequence {
+            length_a = min(self.buffer.len() - self.head - sub_sequence, buffer.len());
+            self.buffer[self.head + sub_sequence..self.head + sub_sequence + length_a]
+                .copy_from_slice(&buffer[..length_a]);
+        }
+
+        // From the begin of the buffer
+        let length_b = buffer.len() - length_a;
+        if length_b > 0 {
+            self.buffer[..length_b].copy_from_slice(&buffer[length_a..]);
+        }
+
+        // Insert and merge ranges
+        {
+            let mut sequence = sequence as u64;
+            if (sequence as u32) < self.sequence {
+                sequence += u32::MAX as u64;
+            }
+
+            // Select ranges which can be merged
+            let mut pop_keys = Vec::new();
+            let mut end = sequence + buffer.len() as u64;
+            for (&key, &value) in self.ranges.range((
+                Included(&sequence),
+                Included(&(sequence + buffer.len() as u64)),
+            )) {
+                pop_keys.push(key);
+                end = max(end, key + value as u64);
+            }
+
+            // Pop
+            for ref pop_key in pop_keys {
+                self.ranges.remove(pop_key);
+            }
+
+            // Select the previous range if exists
+            let mut prev_key = None;
+            for &key in self.ranges.keys() {
+                if key < sequence {
+                    prev_key = Some(key);
+                }
+            }
+
+            // Merge previous range
+            let mut size = buffer.len();
+            if let Some(prev_key) = prev_key {
+                let prev_size = *self.ranges.get(&prev_key).unwrap();
+                if prev_key + (prev_size as u64) >= sequence {
+                    size += (sequence - prev_key) as usize;
+                    sequence = prev_key;
+                }
+            }
+
+            // Insert range
+            self.ranges.insert(sequence, size);
+        }
+
+        // Pop if possible
+        let first_key = *self.ranges.keys().next().unwrap();
+        if first_key as u32 == self.sequence {
+            let size = self.ranges.remove(&first_key).unwrap();
+
+            // Shrink range sequence is possible
+            if ((u32::MAX - self.sequence) as usize) < size {
+                let keys: Vec<_> = self.ranges.keys().map(|x| *x).collect();
+
+                for key in keys {
+                    let value = self.ranges.remove(&key).unwrap();
+                    self.ranges.insert(key - u32::MAX as u64, value);
+                }
+            }
+
+            let mut vector = vec![0u8; size];
+
+            // From the head to the end of the buffer
+            let length_a = min(size, self.buffer.len() - self.head);
+            vector[..length_a].copy_from_slice(&self.buffer[self.head..self.head + length_a]);
+
+            // From the begin of the buffer to the tail
+            let length_b = size - length_a;
+            if length_b > 0 {
+                vector[length_a..].copy_from_slice(&self.buffer[..length_b]);
+            }
+
+            self.sequence = self
+                .sequence
+                .checked_add(size as u32)
+                .unwrap_or_else(|| size as u32 - (u32::MAX - self.sequence));
+            self.head = (self.head + (size % self.buffer.len())) % self.buffer.len();
+
+            return Ok(Some(vector));
+        }
+
+        Ok(None)
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,19 @@
-use std::cmp::{max, min};
-use std::collections::{BTreeMap, HashMap};
+use std::cmp::min;
+use std::collections::HashMap;
 use std::io::{self, Read, Write};
 use std::net::{Ipv4Addr, SocketAddrV4, TcpStream};
-use std::ops::Bound::Included;
 use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::{Arc, Mutex};
 use std::thread::{self, JoinHandle};
 use std::time::{Duration, Instant};
 
 pub mod args;
+pub mod cacher;
 pub mod packet;
 pub mod pcap;
 pub mod socks;
 use crate::socks::SocksDatagram;
+use cacher::{Cacher, RandomCacher};
 use log::{debug, trace, warn, Level, LevelFilter};
 use packet::layer::arp::Arp;
 use packet::layer::ethernet::Ethernet;
@@ -79,125 +80,8 @@ pub fn interface(name: Option<String>) -> Option<Interface> {
     Some(inters[0].clone())
 }
 
-/// Represents the initial size of cache.
-const INITIAL_CACHE_SIZE: usize = 65536;
-/// Represents the max size of cache.
-const MAX_CACHE_SIZE: usize = 1048576;
-
 /// Represents the max distance of u32 values between packets in an u32 window.
 const MAX_U32_WINDOW_SIZE: usize = 4194304;
-
-/// Represents the linear cache.
-pub struct Cacher {
-    buffer: Vec<u8>,
-    sequence: u32,
-    head: usize,
-    size: usize,
-}
-
-impl Cacher {
-    /// Creates a new `Cacher`.
-    pub fn new(sequence: u32) -> Cacher {
-        Cacher {
-            buffer: vec![0; INITIAL_CACHE_SIZE],
-            sequence,
-            head: 0,
-            size: 0,
-        }
-    }
-
-    /// Get the buffer in the given size. The returned vector's length will be no more than the given size.
-    pub fn get(&self, size: usize) -> Option<Vec<u8>> {
-        if self.size == 0 {
-            return None;
-        }
-
-        let length = min(self.size, size);
-        let mut vector = vec![0u8; length];
-
-        // From the head to the end of the buffer
-        let length_a = min(length, self.buffer.len() - self.head);
-        vector[..length_a].copy_from_slice(&self.buffer[self.head..self.head + length_a]);
-
-        // From the begin of the buffer to the tail
-        let length_b = length - length_a;
-        if length_b > 0 {
-            vector[length_a..].copy_from_slice(&self.buffer[..length_b]);
-        }
-
-        Some(vector)
-    }
-
-    /// Appends some bytes to the end of the cache.
-    pub fn append(&mut self, buffer: &[u8]) -> io::Result<()> {
-        if buffer.len() > self.buffer.len() - self.size {
-            // Extend the buffer
-            let size = min(
-                max(self.buffer.len() * 2, self.buffer.len() + buffer.len()),
-                MAX_CACHE_SIZE,
-            );
-            if self.size + buffer.len() > size {
-                return Err(io::Error::new(io::ErrorKind::Other, "cache is full"));
-            }
-
-            trace!("extend cache to {} Bytes", size);
-            warn!("cache is extended: a congestion may be in your network");
-            let mut new_buffer = vec![0u8; size];
-
-            // From the head to the end of the buffer
-            let length_a = min(self.size, self.buffer.len() - self.head);
-            new_buffer[..length_a].copy_from_slice(&self.buffer[self.head..self.head + length_a]);
-
-            // From the begin of the buffer to the tail
-            let length_b = self.size - length_a;
-            if length_b > 0 {
-                new_buffer[length_a..length_a + length_b].copy_from_slice(&self.buffer[..length_b]);
-            }
-
-            self.buffer = new_buffer;
-            self.head = 0;
-        }
-
-        trace!("append {} Bytes to cache", buffer.len());
-
-        // From the tail to the end of the buffer
-        let mut length_a = 0;
-        if self.head + self.size < self.buffer.len() {
-            length_a = min(buffer.len(), self.buffer.len() - (self.head + self.size));
-            self.buffer[self.head + self.size..self.head + self.size + length_a]
-                .copy_from_slice(&buffer[..length_a]);
-        }
-
-        // From the begin of the buffer to the head
-        let length_b = buffer.len() - length_a;
-        if length_b > 0 {
-            self.buffer[..length_b].copy_from_slice(&buffer[length_a..]);
-        }
-
-        self.size += buffer.len();
-
-        Ok(())
-    }
-
-    // Invalidates cache to the certain sequence.
-    pub fn invalidate_to(&mut self, sequence: u32) {
-        trace!("invalidate cache to sequence {}", sequence);
-
-        let size = sequence
-            .checked_sub(self.sequence)
-            .unwrap_or_else(|| u32::MAX - self.sequence + sequence) as usize;
-
-        if size <= MAX_U32_WINDOW_SIZE as usize {
-            self.sequence = sequence;
-            self.size = self.size.checked_sub(size).unwrap_or(0);
-            if self.size == 0 {
-                self.head = 0;
-            } else {
-                self.head = (self.head + (size % self.buffer.len())) % self.buffer.len();
-            }
-        }
-    }
-}
 
 /// Represents the wait time after a `TimedOut` `IoError`.
 const TIMEDOUT_WAIT: u64 = 20;
@@ -205,6 +89,7 @@ const TIMEDOUT_WAIT: u64 = 20;
 /// Represents the MSS of packet sending from local to source, this will become an option in the future.
 const MSS: u32 = 1200;
 
+// TODO: implement rwnd & swnd
 /// Represents the channel downstream traffic to the source in pcap.
 pub struct Downstreamer {
     tx: Sender,
@@ -320,6 +205,7 @@ impl Downstreamer {
         self.send(&indicator)
     }
 
+    /// TODO: update sequence if sequence is larger
     /// Resends TCP ACK packets from the given sequence if necessary.
     pub fn resend_tcp_ack(
         &mut self,
@@ -803,166 +689,6 @@ impl Downstreamer {
     }
 }
 
-/// Represents the random cache.
-pub struct RandomCacher {
-    buffer: Vec<u8>,
-    sequence: u32,
-    head: usize,
-    /// Represents ranges of existing values. Use an u64 instead of an u32 because the sequence is used as a ring.
-    ranges: BTreeMap<u64, usize>,
-}
-
-impl RandomCacher {
-    /// Creates a new `RandomCacher`.
-    pub fn new(sequence: u32) -> RandomCacher {
-        RandomCacher {
-            buffer: vec![0u8; INITIAL_CACHE_SIZE],
-            sequence,
-            head: 0,
-            ranges: BTreeMap::new(),
-        }
-    }
-
-    /// Appends some bytes to the cache and returns continuous bytes from the beginning.
-    pub fn append(&mut self, buffer: &[u8], sequence: u32) -> io::Result<Option<Vec<u8>>> {
-        let sub_sequence = sequence
-            .checked_sub(self.sequence)
-            .unwrap_or_else(|| sequence + (u32::MAX - self.sequence))
-            as usize;
-        if sub_sequence > MAX_U32_WINDOW_SIZE {
-            return Ok(None);
-        }
-
-        let size = sub_sequence + buffer.len();
-        if size > self.buffer.len() {
-            // Extend the buffer
-            let size = min(max(self.buffer.len() * 2, size), MAX_CACHE_SIZE);
-            if self.buffer.len() + buffer.len() > size {
-                return Err(io::Error::new(io::ErrorKind::Other, "cache is full"));
-            }
-
-            trace!("extend cache to {} Bytes", size);
-            warn!("cache is extended: a congestion may be in your network");
-            let mut new_buffer = vec![0u8; size];
-
-            // TODO: the procedure may by optimized to copy valid bytes only
-            // From the head to the end of the buffer
-            new_buffer[..self.buffer.len() - self.head].copy_from_slice(&self.buffer[self.head..]);
-
-            // From the begin of the buffer to the tail
-            if self.head > 0 {
-                new_buffer[self.buffer.len() - self.head..self.buffer.len()]
-                    .copy_from_slice(&self.buffer[..self.head]);
-            }
-
-            self.buffer = new_buffer;
-            self.head = 0;
-        }
-
-        trace!("append {} Bytes to cache", buffer.len());
-
-        // TODO: the procedure may by optimized to copy valid bytes only
-        // To the end of the buffer
-        let mut length_a = 0;
-        if self.buffer.len() - self.head > sub_sequence {
-            length_a = min(self.buffer.len() - self.head - sub_sequence, buffer.len());
-            self.buffer[self.head + sub_sequence..self.head + sub_sequence + length_a]
-                .copy_from_slice(&buffer[..length_a]);
-        }
-
-        // From the begin of the buffer
-        let length_b = buffer.len() - length_a;
-        if length_b > 0 {
-            self.buffer[..length_b].copy_from_slice(&buffer[length_a..]);
-        }
-
-        // Insert and merge ranges
-        {
-            let mut sequence = sequence as u64;
-            if (sequence as u32) < self.sequence {
-                sequence += u32::MAX as u64;
-            }
-
-            // Select ranges which can be merged
-            let mut pop_keys = Vec::new();
-            let mut end = sequence + buffer.len() as u64;
-            for (&key, &value) in self.ranges.range((
-                Included(&sequence),
-                Included(&(sequence + buffer.len() as u64)),
-            )) {
-                pop_keys.push(key);
-                end = max(end, key + value as u64);
-            }
-
-            // Pop
-            for ref pop_key in pop_keys {
-                self.ranges.remove(pop_key);
-            }
-
-            // Select the previous range if exists
-            let mut prev_key = None;
-            for &key in self.ranges.keys() {
-                if key < sequence {
-                    prev_key = Some(key);
-                }
-            }
-
-            // Merge previous range
-            let mut size = buffer.len();
-            if let Some(prev_key) = prev_key {
-                let prev_size = *self.ranges.get(&prev_key).unwrap();
-                if prev_key + (prev_size as u64) >= sequence {
-                    size += (sequence - prev_key) as usize;
-                    sequence = prev_key;
-                }
-            }
-
-            // Insert range
-            self.ranges.insert(sequence, size);
-        }
-
-        // Pop if possible
-        let first_key = *self.ranges.keys().next().unwrap();
-        if first_key as u32 == self.sequence {
-            let size = self.ranges.remove(&first_key).unwrap();
-
-            // Shrink range sequence is possible
-            if ((u32::MAX - self.sequence) as usize) < size {
-                let keys: Vec<_> = self.ranges.keys().map(|x| *x).collect();
-
-                for key in keys {
-                    let value = self.ranges.remove(&key).unwrap();
-                    self.ranges.insert(key - u32::MAX as u64, value);
-                }
-            }
-
-            let mut vector = vec![0u8; size];
-
-            // From the head to the end of the buffer
-            let length_a = min(size, self.buffer.len() - self.head);
-            vector[..length_a].copy_from_slice(&self.buffer[self.head..self.head + length_a]);
-
-            // From the begin of the buffer to the tail
-            let length_b = size - length_a;
-            if length_b > 0 {
-                vector[length_a..].copy_from_slice(&self.buffer[..length_b]);
-            }
-
-            self.sequence = self
-                .sequence
-                .checked_add(size as u32)
-                .unwrap_or_else(|| size as u32 - (u32::MAX - self.sequence));
-            self.head = (self.head + (size % self.buffer.len())) % self.buffer.len();
-
-            trace!("pop cache to sequence {}", self.sequence);
-
-            return Ok(Some(vector));
-        }
-
-        Ok(None)
-    }
-}
-
 /// Represents the initial UDP port for binding in local.
 const INITIAL_PORT: u16 = 32768;
 /// Represents the max limit of UDP port for binding in local.
@@ -1226,9 +952,11 @@ impl Upstreamer {
                             if *self.tcp_acknowledgement_count_map.get(&key).unwrap_or(&0) > 3
                                 && !tcp.is_zero_window()
                             {
+                                // Fast retransmit
                                 let mut is_retransmit = true;
                                 if let Some(instant) = self.tcp_retransmission_map.get(&key) {
                                     if instant.elapsed().as_millis() < 20 {
+                                        // Cooled down
                                         is_retransmit = false;
                                     }
                                 }
